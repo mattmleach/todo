98b6f0be9d5fd0040eda85ce7753e3cff076f76d
Matt Leach <MLeach@symprod.com>
2019-01-10 17:21:25 -0500

    Express Init

--- server/server.js
+++ server/server.js
@@ -1,26 +1,12 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-var Todo = mongoose.model('Todo', {
-    text: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    },
-    completedAt: {
-        type: Number
-    }
-});
+var app = express();
 
-var newTodo = new Todo({
-    text: 'Cook dinner'
-});
-
-newTodo.save().then((doc) => {
-    console.log('Saved todo', doc);
-}, (e) => {
-    console.log('Unable to save todo.');
+app.listen(3000, () => {
+    console.log('Started on port 3000');
 });
