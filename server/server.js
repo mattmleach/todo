d6b391f4d520736a9925ccb91f1de280e7f8a13f
Matt Leach <MLeach@symprod.com>
2019-01-11 16:25:47 -0500

    Post added

--- server/server.js
+++ server/server.js
@@ -7,6 +7,12 @@ var {User} = require('./models/user');
 
 var app = express();
 
+app.use(bodyParser.json());
+
+app.post('/todos',(req, res) => {
+    console.log(req.body);
+});
+
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
