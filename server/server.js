f655e6a0f3336430164882ef3373e628ab8b2ecd
Matt Leach <MLeach@symprod.com>
2019-01-14 10:40:29 -0500

    Add post /todos route

--- server/server.js
+++ server/server.js
@@ -10,7 +10,14 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos',(req, res) => {
-    console.log(req.body);
+    let todo = new Todo({
+        text: req.body.text
+    });
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
 });
 
 app.listen(3000, () => {
