4cafed0177960031a42017f6b42efdc72c95dfaf
Matt <mattmleach@gmail.com>
2019-01-23 15:52:10 -0500

    Test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -50,7 +50,7 @@ app.get('/todos/:id', (req, res) => {
 
 app.delete('/todos/:id', (req, res) => {
     const id = req.params.id;
-
+    
     if(!ObjectID.isValid(id)) {
         return res.status(404).send();
     }
@@ -59,7 +59,7 @@ app.delete('/todos/:id', (req, res) => {
         if(!todo) {
             return res.status(404).send();
         }
-        res.send(todo);
+        res.send({todo});
     }).catch((e) => {
         res.status(400).send();
     });
